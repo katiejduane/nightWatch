6a226a4af68ec0f3ebfcbae6d34c48d88c054525
Katie Duane <katiejduane@gmail.com>
2019-06-24 23:19:12 -0400

    still practicing on emoceans

--- tests/katiesFirstAttempt.js
+++ tests/katiesFirstAttempt.js
@@ -9,7 +9,7 @@ module.exports = {
 }
 
 module.exports = {
-    "Visits and logs into emOceans"(client){
+    'Step 1: visits and logs into emOceans': function(client){
         client
             .url('https://emoceans.katiejduane.com')
             .waitForElementVisible('.header-content')
@@ -19,13 +19,57 @@ module.exports = {
             .waitForElementVisible('.form-login')
             .setValue('#email', 'katiejduane@gmail.com')
             .setValue('#password', 'x')
+            .pause(1000)
+    },
+    'Step 2: clicks on and adjusts a color and advances to form page': function(client){
+        client  
             .click('.targBoxlogin')
             .waitForElementVisible('.swatchContainer')
             .click('.swatch')
             .click('#lighten')
+            .pause(500)
             .click('#desat')
+            .pause(500)
+            .click('#sat')
+            .pause(500)
+            .click('#warm')
+            .pause(500)
+            .click('#cool')
+            .pause(500)
+            .assert.cssProperty('#palette', 'background-color', 'rgba(182, 88, 106, 1)')
             .click('#save')
-            .waitForElementVisible('.submitContainer')
-        client.end();
+    },
+    'Step 2b: loads form page and enters word in search bar': function(client){
+        client
+            .waitForElementVisible('#moodForm')
+            .setValue('#search-input', 'happy')
+            .pause(1000)
+    },
+    'Step 3: submits and waits for API response': async function(client){
+        await client.click('.datamuse-button')
+    },
+    'Step 4: enters mood data in form and sends to DB': function(client){
+        client  
+            .setValue('#mood', 'Glorious')
+            .setValue('#note', 'This is rather fun')
+            .pause(3000)
+            .click('#submitButton')
+    },
+    'Step 5: views mood data': function(client){
+        client
+            .waitForElementVisible('.boardDisplay')
+            .pause(3000)
+            .click('#viewYear')
+            .pause(1000)
+            .click('.byYear')
+            .pause(1000)
+            .assert.elementPresent('.modal-word')
+            // .moveTo(null, 200, 200)
+            // .click('.back-drop')
+            .end()
     }
 }
+
+
+// move mouse away from modal before clicking backdrop, then see if you can click on elements of
+// a specific bg color
