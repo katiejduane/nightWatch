cb9cb70d9801014721c0f970da1757ac5df4eee6
Katie Duane <katiejduane@gmail.com>
2019-06-25 09:58:19 -0400

    boomRoasted

--- tests/katiesFirstAttempt.js
+++ tests/katiesFirstAttempt.js
@@ -1,12 +1,12 @@
-module.exports = {
-    "Visits Katie's portfolio site"(client){
-        client
-            .url('https://katiejduane.com')
-            .waitForElementVisible('.projects-box')
-            .expect.element('.project').to.be.present.after(1000)
-        client.end();
-    }
-}
+// module.exports = {
+//     "Visits Katie's portfolio site"(client){
+//         client
+//             .url('https://katiejduane.com')
+//             .waitForElementVisible('.projects-box')
+//             .expect.element('.project').to.be.present.after(1000)
+//         client.end();
+//     }
+// }
 
 module.exports = {
     'Step 1: visits and logs into emOceans': function(client){
@@ -27,15 +27,15 @@ module.exports = {
             .waitForElementVisible('.swatchContainer')
             .click('.swatch')
             .click('#lighten')
-            .pause(500)
+            .pause(1000)
             .click('#desat')
-            .pause(500)
+            .pause(1000)
             .click('#sat')
-            .pause(500)
+            .pause(1000)
             .click('#warm')
-            .pause(500)
+            .pause(1000)
             .click('#cool')
-            .pause(500)
+            .pause(1000)
             .assert.cssProperty('#palette', 'background-color', 'rgba(182, 88, 106, 1)')
             .click('#save')
     },
@@ -47,10 +47,11 @@ module.exports = {
     },
     'Step 3: submits and waits for API response': async function(client){
         await client.click('.datamuse-button')
+        client.waitForElementVisible('.results', 6000, false)
     },
     'Step 4: enters mood data in form and sends to DB': function(client){
         client  
-            .setValue('#mood', 'Glorious')
+            .setValue('#mood', 'Joyful')
             .setValue('#note', 'This is rather fun')
             .pause(3000)
             .click('#submitButton')
@@ -59,13 +60,26 @@ module.exports = {
         client
             .waitForElementVisible('.boardDisplay')
             .pause(3000)
+            .click('#viewMonth')
+            .pause(1000)
             .click('#viewYear')
             .pause(1000)
             .click('.byYear')
             .pause(1000)
             .assert.elementPresent('.modal-word')
-            // .moveTo(null, 200, 200)
-            // .click('.back-drop')
+            .assert.elementPresent('.modal-date')
+    },
+    'Step 6: views about info and then logs out': function(client){
+        client  
+            .useXpath()
+            .click("//a[text()='about']")
+            .useCss()
+            .waitForElementVisible('#aboutColorWrapper')
+            .moveToElement('#devWrapper', 100, 100)
+            .useXpath()
+            .pause(1000)
+            .click("//a[text()='log out']")
+            .pause(2000)
             .end()
     }
 }
